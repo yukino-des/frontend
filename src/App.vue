27857fee438329efbe29c0e8c2dd2bf3e0e36df1
yukino <3181137349@qq.com>
2023-05-23 15:41:20 +0800

    frontend-proj4pdd

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <app-content></app-content>
+  </div>
+</template>
+
+<script>
+import content from "./components/content"
+
+export default {
+  name: "PlantDiseaseDetection",
+  components: {
+    "app-content": content
+  },
+}
+</script>
